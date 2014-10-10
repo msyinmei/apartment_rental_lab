4a9e3f4b2314f915a7caf0ea7a9cb2f2288618e1
msyinmei <yin.codes@gmail.com>
2014-10-10 15:40:26 -0700

    phaseII done

--- src/main.js
+++ src/main.js
@@ -5,6 +5,40 @@ var app = require('./app.js');
 var building = new app.Building("Waterfront Tower");
 var people = [];
 
+var findPerson = function (pName) {
+  for (var i = 0; i < people.length; i++){
+    if (people[i].name === pName) {
+      return people[i];
+    }
+  }
+  return null;
+};
+
+var findUnit = function (unit_number){
+  for (var i = 0; i < building.units.length; i++){
+    if (building.units[i].number == unit_number) {
+      return building.units[i];
+    }
+  }
+  return null;
+};
+
+// Add some seed data
+
+people.push(new app.Person("Anna", "765-4321"));
+var john = new app.Manager("John", "700-4321");
+building.setManager(john);
+people.push(john);
+var devin = new app.Tenant("Devin", "765-1234");
+devin.addReference(new app.Person("Carl", "415 3536 222"));
+devin.addReference(new app.Person("Steve", "415 1111 222"));
+people.push(devin);
+people.push(new app.Tenant("Steve", "744-1234"));
+
+building.units.push(new app.Unit("12", building, 400, 2000));
+building.units.push(new app.Unit("13", building, 800, 3000));
+building.units.push(new app.Unit("14", building, 1800, 4500));
+
 // --------------------------------
 menu.addDelimiter('-', 40, building.address + " rental app");
 
@@ -36,7 +70,7 @@ menu.addItem('Show tenants:',
         if(!references) {continue;}
         for (var j = references.length - 1; j >= 0; j--) {
           console.log("-> Reference: " + references[j].name + " " + references[j].contact);
-        };
+        }
       }
     }
   }
@@ -56,23 +90,66 @@ menu.addItem('Add unit',
 menu.addItem('Show all units',
   function() {
     for(var i = building.units.length - 1; i >= 0; i--) {
+      if (building.units[i].tenant !== null){
+          console.log(" tenant: " + building.units[i].tenant.name +
+          " num: " + building.units[i].number +
+          " sqft: " + building.units[i].sqft +
+          " rent: $" + building.units[i].rent);
+      } else {
       console.log(" tenant: " + building.units[i].tenant +
       			  " num: " + building.units[i].number +
                   " sqft: " + building.units[i].sqft +
                   " rent: $" + building.units[i].rent);
     }
+    }
   }
 );
 
-menu.addItem('(implement me) Show available units',
+menu.addItem('Show available units',
   function() {
-      console.log("Implement me");
+    for(var i = building.units.length - 1; i >= 0; i--) {
+        if (building.units[i].tenant === null) {
+      console.log(" tenant: " + building.units[i].tenant +
+              " num: " + building.units[i].number +
+                  " sqft: " + building.units[i].sqft +
+                  " rent: $" + building.units[i].rent);
+        }
     }
+  }
 );
 
-menu.addItem('(implement me) Add tenant reference',
+menu.addItem('Add tenant reference',
   function(tenant_name, ref_name, ref_contact) {
-      console.log("Implement me. Show error if tenant is unknown. Note: a reference is a person");
+      var tenant = findPerson(tenant_name);
+      // console.log (tenant);
+      if (tenant instanceof app.Tenant){
+        var ref = findPerson(ref_name);
+          // if (ref === undefined) {
+            ref = new app.Person(ref_name, ref_contact);
+            people.push(ref);
+            tenant.addReference(ref);
+          // } else {
+          //   tenant.addReference(ref);
+          // }
+        console.log('Success! tenant: ' + tenant_name +
+              ' reference: ' + ref_name +
+              ' ref contact: ' + ref_contact);
+      } else {
+        console.log ('Error: tenant does not exist.');
+      }
+
+      // for (var i = 0; i <= people.length; i++) {
+      //   if (people[i] instanceof app.Tenant){
+      //     if (people[i].name === tenant_name) {
+      //         people[i].addReference(new app.Person(ref_name, ref_contact));
+      //         console.log('Success! tenant: ' + tenant_name +
+      //         ' reference: ' + ref_name +
+      //         ' ref contact: ' + ref_contact);
+      //     } else {
+      //       console.log ('Error: tenant does not exist.');
+      //     }
+      //   }
+      // }
     },
     null,
     [{'name': 'tenant_name', 'type': 'string'},
@@ -80,42 +157,86 @@ menu.addItem('(implement me) Add tenant reference',
     {'name': 'ref_contact', 'type': 'string'}]
 );
 
-menu.addItem('(implement me) Move tenant in unit',
+menu.addItem('Move tenant in unit',
   function(unit_number, tenant_name) {
       // find tenant and unit objects, use building's addTenant() function.
-      console.log("Implement me.");
-    },
+      var tenant = findPerson(tenant_name);
+      var unit = findUnit(unit_number);
+      console.log('Unit: ' + unit);
+      console.log('Tenant: '+ tenant);
+      if (unit.available() === true){
+        building.addTenant(unit, tenant);
+        console.log(tenant_name + " is now the tenant of unit#" + unit_number);
+      } else {
+        console.log("Unit is not available or tenant is non-existent");
+      }
+      },
     null,
     [{'name': 'unit_number', 'type': 'string'},
     {'name': 'tenant_name', 'type': 'string'}]
 );
 
-menu.addItem('(implement me) Evict tenant',
+menu.addItem('Evict tenant',
   function(tenant_name) {
-      // Similar to above, use building's removeTenant() function.
-      console.log("Implement me");
-    },
+    var tenant = findPerson(tenant_name);
+    if (tenant !== null){
+      for (var i = 0; i < building.units.length; i++){
+        if (building.units[i].tenant === tenant) {
+        var unit = building.units[i];
+            building.removeTenant(unit, tenant);
+            console.log("We have evicted " + tenant_name + " from unit#" + building.units[i].number);
+          }
+        }
+    } else {
+          console.log("the person you requested is not a tenant");
+    }
+  },
     null,
     [{'name': 'tenant_name', 'type': 'string'}]
 );
 
-menu.addItem('(implement me) Show total sqft rented',
+menu.addItem('Show total sqft rented',
   function() {
-      console.log("Implement me");
+    var total = 0;
+    for(var i = building.units.length - 1; i >= 0; i--){
+      if (building.units[i].tenant !== null){
+        total += building.units[i].sqft;
+        console.log(" sqft:" + total);
+      }
     }
+    // var add = function(a,b){return a + b;};
+    //   for (var i = 0; i < building.units.length; i++){
+    //     if (building.units[i].available === false) {
+    //       rentedUnits.push(building.units[i].sqft);
+    //     }
+    // console.log(rentedUnits.reduce(add()));
+    //   }
+    //   null;
+  }
 );
 
-menu.addItem('(implement me) Show total yearly income',
+menu.addItem('Show total yearly income',
   function() {
-      // Note: only rented units produce income
-      console.log("Implement me.");
-    }
+  var total = 0;
+  for(var i = building.units.length - 1; i >= 0; i--) {
+        if (building.units[i].tenant !== null){
+        total += building.units[i].rent;
+        console.log(" rent: $" + total);
+          }
+        }
+       }
 );
 
-menu.addItem('(Add your own feature ...)',
+menu.addItem('Show total number of units rented',
   function() {
-      console.log("Implement a feature that you find is useful");
-    }
+  var total = 0;
+  for(var i = building.units.length - 1; i >= 0; i--) {
+        if (building.units[i].tenant !== null){
+        total += 1;
+        console.log("total units rented: " + total);
+          }
+        }
+       }
 );
 
 // *******************************
